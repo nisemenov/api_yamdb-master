c899a28f460012c520f095502521cf2897c0c0db
Nikita Semenov <nasemenov726@gmail.com>
2023-08-24 17:59:11 +0300

    Models register

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Review, Category, Title, Genre
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "pub_date", "author", "score")
+    search_fields = ("text",)
+    list_filter = ("pub_date",)
+    empty_value_display = "-empty-"
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'category', 'genre')
+    search_fields = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
