741724a9fb93ca087066d95dafb3240c1dbe218f
Nikita Semenov <nasemenov726@gmail.com>
2023-08-24 17:31:32 +0300

    Modified some models

--- review/models.py
+++ review/models.py
@@ -7,38 +7,67 @@ User = get_user_model()
 class Review(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Publication date', auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name='review')
-    title = models.ForeignKey('Title', on_delete=models.CASCADE,
-                              blank=True, null=True,
-                              related_name='review',
-                              verbose_name='titles')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='review'
+    )
+    title_id = models.ForeignKey(
+        'Title',
+        on_delete=models.CASCADE,
+        related_name='review'
+    )
+    score = models.IntegerField()
 
     def __str__(self):
         return self.text
 
 
 class Title(models.Model):
-
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='title'
+    )
+    genre = models.ForeignKey(
+        'Genre',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='title'
+    )
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="comments")
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
     text = models.TextField()
-    created = models.DateTimeField('Date of create',
-                                   auto_now_add=True, db_index=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comment'
+    )
+    pub_date = models.DateTimeField('Publication date', auto_now_add=True)
+    review_id = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comment'
+    )
 
 
 class Category(models.Model):
-    title = models.CharField('Title', max_length=200)
+    name = models.CharField(max_length=200)
     slug = models.SlugField('Slug', blank=True, null=True)
-    description = models.TextField('Description', blank=True, null=True)
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
 class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField('Slug', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
